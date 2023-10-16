0a461973c518cff62fa393f769c83d839f593175
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-16 16:29:03 +0800

    猜数游戏:生成随机数字

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,12 @@
-use std::io;
+use std::io; // prelude
+use rand::Rng; // trait - 类似接口，里面定义了很多方法
 
 fn main() {
     println!("猜数！");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101); // 生成一个1-100之间的随机数
+
+    println!("生成神秘数字是：{}", secret_number);
 
     println!("猜测一个数");
 
