ecbc7e22fa6480b6df5bd720c907440cf96ab2e3
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-17 09:44:49 +0800

    函数

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,20 @@
 fn main() {
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    // let x = 5;
 
-    let (x, y, z) = tup; // 解构
+    /* let y = { // y就是等于x+3的值
+        let x = 1;
+        x + 3
+    }; */
 
-    println!("{}, {}, {}", tup.0, tup.1, tup.2);
+    let y = plus_five(6);
 
-    println!("{}, {}, {}", x, y, z);
+    println!("The value of y is: {}", y);
 }
+
+fn plus_five(x: i32) -> i32 {
+    x + 5 // 不能加分号，否则就是语句不是表达式了
+}
+
+// 在 -> 符号后边声明函数返回值的类型，但是不可以为返回值命名
+// 返回值就是函数体里面最后一个表达式的值
+// 若想提前返回，需使用return关键字并指定一个值
