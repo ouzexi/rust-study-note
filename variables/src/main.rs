cfe5e92857a909b82ee03cd984f1b0e16b8476ea
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-17 09:58:34 +0800

    控制流:循环

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,25 @@
 fn main() {
-    let condition = true;
+    let mut counter = 0;
 
-    let y = if condition { 5 } else { 6 };
+    let result = loop {
+        counter += 1;
 
-    println!("{}", y);
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is: {}", result); // 20
+
+    //
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("the value is： {}", element);
+    }
+
+    for number in (1..4).rev() { // ..是Range，1到3 rev是反转
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!");
 }
