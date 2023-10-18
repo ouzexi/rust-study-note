ccbfb08c8be9a036bfc128b0e3945a5fe07f4e9c
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-18 16:59:03 +0800

    Vector+Enum

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -1,11 +1,13 @@
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+
 fn main() {
-    let mut v = vec![100, 32, 57];
-
-    for i in &mut v {
-        *i += 50;
-    }
-
-    for i in v {
-        println!("{}", i);
-    }
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
 }
