8d1c952f9670085b7af3fdc6ab98a53b8af65978
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-18 15:26:49 +0800

    use as 关键字

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -1,11 +1,8 @@
-mod front_of_house {
-    pub mod hosting { // 函数、方法、mod、变量等默认是私有的，要使用pub暴露
-        pub fn add_to_waitlist() {}
-    }
-}
+use std::fmt::Result;
+use std::io::Result as IoResult;
 
-pub fn eat_at_restaurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
+fn f1() -> Result {}
 
-    front_of_house::hosting::add_to_waitlist();
-}
+fn f2() -> IoResult {}
+
+fn main() {}
