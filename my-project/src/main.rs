776b0854116a24496f9b5f1605848c0cae51f94d
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-18 17:29:45 +0800

    String

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -1,13 +1,14 @@
-enum SpreadsheetCell {
-    Int(i32),
-    Float(f64),
-    Text(String),
-}
-
 fn main() {
-    let row = vec![
-        SpreadsheetCell::Int(3),
-        SpreadsheetCell::Text(String::from("blue")),
-        SpreadsheetCell::Float(10.12),
-    ];
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{}-{}-{}", s1, s2, s3);
+    println!("{}", s);
+
+    let mut s4 = String::from("foo");
+    let s5 = String::from("bar");
+    s4.push_str(&s5);
+
+    println!("{}", s4);
 }
