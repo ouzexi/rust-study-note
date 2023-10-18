8bc13e61c2d1307f9e74484a56301a2bebda8643
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-18 09:10:28 +0800

    match

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
-    let some_number = Some(5);
-    let some_string = Some("A String");
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+}
 
-    let absent_number: Option<i32> = None;
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
 }
