9f0f21fff07924a1e3112300c393211eefe973b4
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-18 09:14:57 +0800

    if let

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,12 +1,15 @@
 fn main() {
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
-}
-
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        None => None,
-        Some(i) => Some(i + 1),
+    let v = Some(0u8);
+    match v {
+        Some(3) => println!("three"),
+        _ => println!("others"),
     }
+
+    // if let只针对一种模式匹配，等价上面
+    if let Some(3) = v {
+        println!("three");
+    } else {
+        println!("others");
+    }
+
 }
