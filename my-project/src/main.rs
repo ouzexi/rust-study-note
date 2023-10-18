fb0fa8fb2e4b52bdbf781a82a0d39637a0b088be
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-18 17:40:53 +0800

    遍历String

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -1,14 +1,7 @@
 fn main() {
-    let s1 = String::from("tic");
-    let s2 = String::from("tac");
-    let s3 = String::from("toe");
+    let w = "水水水水";
 
-    let s = format!("{}-{}-{}", s1, s2, s3);
-    println!("{}", s);
-
-    let mut s4 = String::from("foo");
-    let s5 = String::from("bar");
-    s4.push_str(&s5);
-
-    println!("{}", s4);
+    for b in w.chars() {
+        println!("{}", b);
+    }
 }
