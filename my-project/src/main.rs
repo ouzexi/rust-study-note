74e53aeef980c1a2550bd162d18a24bccecd0992
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-18 09:36:58 +0800

    路径

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -1,12 +1,11 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
-        fn seat_at_table() {}
+    pub mod hosting { // 函数、方法、mod、变量等默认是私有的，要使用pub暴露
+        pub fn add_to_waitlist() {}
     }
-    
-    mod serving {
-        fn take_order() {}
-        fn serve_order() {}
-        fn take_payment() {}
-    }
+}
+
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    front_of_house::hosting::add_to_waitlist();
 }
