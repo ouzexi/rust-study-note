676ddddf807a2e823f2043affca4b57186696a66
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-19 11:43:42 +0800

    Result枚举与可恢复的错误

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -1,8 +1,38 @@
-use std::collections::HashMap;
+use std::{fs::File, io::ErrorKind};
 
 fn main() {
-    let teams = vec![String::from("Blue"), String::from("Yellow")];
-    let initial_scores = vec![10, 50];
+    let f = File::open("hello.txt");
 
-    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("Error opening file {:?}", error);
+        }
+    };
+
+
+    // 使用Result
+    let f1 = File::open("hello.txt");
+
+    let f1 = match f1 {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.text") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Error creating file {:?}", e),
+            },
+            other_error => panic!("Error opening the file:{:?}", other_error),
+        }
+    };
+
+    // 使用unwrap方法代替Result
+    let f2 = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Error creating file: {:?}", error);
+            })
+        } else {
+            panic!("Error opening file: {:?}", error);
+        }
+    });
 }
