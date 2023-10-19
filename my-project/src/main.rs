7945393ce618b21de296db3a587e14c98d7aefe2
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-19 17:45:02 +0800

    Trait作为函数参数

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 pub trait Summary {
     fn summarize(&self) -> String {     // trait中方法的默认实现
         String::from("(Read more...)")
@@ -28,6 +30,16 @@ impl Summary for Tweet {
     fn summarize(&self) -> String {
         format!("{}: {}", self.username, self.content)
     }
+}
+
+// trait作为函数参数 且 实现多个trait
+pub fn notify1(item1: impl Summary + Display) {
+    println!("Breaking news! {}", item1.summarize());
+}
+
+// 可以使用泛型简化参数类型
+pub fn notify<T: Summary + Display>(item1: T, item2: T) {
+    println!("Breaking news! {}", item1.summarize());
 }
 
 fn main() {
