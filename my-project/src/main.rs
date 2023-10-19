251909e5422b9d6c84fa32366a1b0ac66f208fcc
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-19 10:16:57 +0800

    HashMap

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -1,7 +1,8 @@
-fn main() {
-    let w = "水水水水";
+use std::collections::HashMap;
 
-    for b in w.chars() {
-        println!("{}", b);
-    }
+fn main() {
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
 }
