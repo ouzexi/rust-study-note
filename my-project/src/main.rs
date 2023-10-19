0524c6065283310d31e9e7aa55c254201471a47c
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-19 16:19:28 +0800

    泛型

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -1,19 +1,21 @@
-fn largest(list: &[i32]) -> i32 {
-    let mut largest = list[0];
-    for &item in list {
-        if item > largest {
-            largest = item;
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
         }
     }
-    largest
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    let p1 = Point { x: 5, y: 4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+    let p3 = p1.mixup(p2);
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
