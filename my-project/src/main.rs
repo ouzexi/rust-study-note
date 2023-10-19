39cf44df261d66b2e2c4df2fc3aba715549e6fba
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-19 15:36:29 +0800

    提取函数以消除重复代码

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -1,30 +1,19 @@
-// 创建新的类型，把验证逻辑放在构造实例的函数里
-pub struct Guess {
-    value: i32,
-}
-
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {}", value);
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+    for &item in list {
+        if item > largest {
+            largest = item;
         }
-
-        Guess { value }
     }
-
-    pub fn value(&self) -> i32 {
-        self.value
-    }
+    largest
 }
 
 fn main() {
-    loop {
-        let guess = "32";
-        let guess: i32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 
-        let guess = Guess::new(guess);
-    }
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 }
