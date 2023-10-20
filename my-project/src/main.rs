0618061a613bfc81da441001894ff1ac9cefed24
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-20 09:20:25 +0800

    生命周期

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -1,54 +1,17 @@
-use std::fmt::Display;
-
-pub trait Summary {
-    fn summarize(&self) -> String {     // trait中方法的默认实现
-        String::from("(Read more...)")
-    }
-}
-
-pub struct  NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
-
-impl Summary for NewsArticle {
-    /* fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
-    } */
-}
-
-pub struct Tweet {
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub retweet: bool,
-}
-
-impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
-    }
-}
-
-// trait作为函数参数 且 实现多个trait
-pub fn notify1(item1: impl Summary + Display) {
-    println!("Breaking news! {}", item1.summarize());
-}
-
-// 可以使用泛型简化参数类型
-pub fn notify<T: Summary + Display>(item1: T, item2: T) {
-    println!("Breaking news! {}", item1.summarize());
-}
-
 fn main() {
-    let article = NewsArticle {
-        headline: String::from("headline"),
-        content: String::from("contenttttt"),
-        author: String::from("author"),
-        location: String::from("location"),
-    };
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
 
-    print!("1 new tweet: {}", article.summarize())
+    let result = longest(string1.as_str(), string2);
+
+    println!("The longest string is {}", result);
+}
+
+// 'a是声明一个生命周期，表示返回值和函数参数一样的生命周期
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
