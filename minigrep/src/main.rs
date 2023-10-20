d6fa431ac8ee87a6160ec15e627b6048d6751799
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-20 17:18:17 +0800

    实例:接收命令行参数

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+// cargo run abcd readme.txt 实现简易的grep命令，在readme.txt查找adbc字符串
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Search for {}", query);
+    println!("In file {}", filename);
 }
