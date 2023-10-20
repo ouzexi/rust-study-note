ac917c1442b45c48b19e0e47df78743cbfff1f2c
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-20 18:06:55 +0800

    重构-将业务逻辑移至lib.rs

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,5 @@
+use minigrep::Config;
 use std::env;
-use std::fs;
 use std::process;
 // cargo run abcd readme.txt 实现简易的grep命令，在readme.txt查找adbc字符串
 fn main() {
@@ -10,26 +10,9 @@ fn main() {
         process::exit(1); // 退出程序
     });
 
-    let contents = fs::read_to_string(config.filename)
-    .expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
 }
+
