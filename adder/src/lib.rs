7a48e102350b1f3ab49ac3e704e16894359af5c6
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-20 16:06:42 +0800

    并行和连续执行测试

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,11 +1,21 @@
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() -> Result<(), String> {
-        if 2 + 2 == 4 {
-            Ok(())
-        } else {
-            Err(String::from("two plus two does not equal four"))
-        }
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
     }
 }
