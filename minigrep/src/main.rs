3daa2af717857e54becb3dd80669e54a2204997d
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-20 17:57:19 +0800

    重构-错误处理

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 // cargo run abcd readme.txt 实现简易的grep命令，在readme.txt查找adbc字符串
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {  // |err|是闭包
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1); // 退出程序
+    });
 
     let contents = fs::read_to_string(config.filename)
     .expect("Something went wrong reading the file");
@@ -18,10 +22,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
