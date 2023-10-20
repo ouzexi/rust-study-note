69f1eecad1f5b0c197aad4639171a60f2f5d1bbd
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-20 17:27:08 +0800

    实例:读取文件

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 // cargo run abcd readme.txt 实现简易的grep命令，在readme.txt查找adbc字符串
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,4 +9,9 @@ fn main() {
 
     println!("Search for {}", query);
     println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+    .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
