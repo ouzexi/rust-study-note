2f7427f5987c065e3d1582e1da1b3331fde449e7
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-20 14:52:09 +0800

    在测试中使用Result

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,26 +1,11 @@
-pub struct Guess {
-    value: u32,
-}
-
-impl Guess {
-    pub fn new(value: u32) -> Guess {
-        if value < 1 {
-            panic!("Guess value must be greater than or equal to 1, got {}.", value)
-        } else if value > 100 {
-            panic!("Guess value must be less than or equal to 100, got {}.", value)
-        }
-
-        Guess { value }
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
-    #[should_panic(expected = "Guess value must be less than or equal to 100")] // panic包含这段错误信息
-    fn greater_than_100() {
-        Guess::new(200);
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
     }
 }
