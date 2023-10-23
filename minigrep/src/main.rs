630ee0a2e544c694cb5e3fbd2d6886b77bca0e81
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-23 10:49:13 +0800

    闭包:使用泛型和Fn Trait存储闭包

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,27 +1,51 @@
 use std::{thread, time::Duration};
 
-fn main() {
-    let example_closure = |x| x; // 相当于js的(x) => x;
+struct Cacher<T>
+where T: Fn(u32) -> u32 {
+    calculation: T,
+    value: Option<u32>,
+}
 
-    let s = example_closure(String::from("hello"));
-    let n = example_closure(5); // 这个会报错，因为上面已经推断x为String类型了
+impl<T> Cacher<T> where T: Fn(u32) -> u32 {
+    // 构造函数
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
+fn main() {
+    generate_workout(24, 4);
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_closure = |num| { // 匿名函数，参数要用||包裹
+    let mut expensive_closure = Cacher::new(|num| { // 匿名函数，参数要用||包裹
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2)); // 模拟耗时操作
         num
-    };
+    });
 
     if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_closure(intensity)); // 使用闭包
-        println!("Next, do {} situps!", expensive_closure(intensity));
+        println!("Today, do {} pushups!", expensive_closure.value(intensity)); // 使用闭包
+        println!("Next, do {} situps!", expensive_closure.value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!"); // 这里就不需要调用耗时函数了
         } else {
-            println!("Today, run for {} minutes!", expensive_closure(intensity));
+            println!("Today, run for {} minutes!", expensive_closure.value(intensity));
         }
     }
 }
