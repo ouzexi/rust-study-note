26021e11ae0296627d610c2b535f8ec1c547a246
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-25 16:25:59 +0800

    Drop Trait

--- my_box/src/main.rs
+++ my_box/src/main.rs
@@ -1,32 +1,26 @@
-use std::ops::Deref;
-
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
+struct CustomSmartPointer {
+    data: String,
 }
-
-impl<T> Deref for MyBox<T> {  // 要使用*y引用，要实现Deref trait（类似于java重写接口的方法）
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        &self.0 // 把self元组的第0个元素返回
+// 实现Drop Trait，可以自定义当值将要离开作用域时发生的动作，一般用于文件，网络资源释放
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
     }
-}
-
-fn hello(name: &str) {
-    println!("Hello, {}", name);
 }
 
 fn main() {
-    let m = MyBox::new(String::from("Rust"));
-
-    // hello函数接收参数的类型是字符串切片的引用，但是这里可以传MyBox<String>的引用
-    // 是因为Deref Trait自动拆箱的过程
-    // &m = &MyBox<String> -> deref &String -> deref &str 
-    hello(&m);
-
-    hello("Rust");
+    let c = CustomSmartPointer{ data: String::from("my stuff") };
+    drop(c); // 主动调用drop后，不会在离开main时被2次调用
+    let d = CustomSmartPointer { data: String::from("other stuff") };
+    println!("CustomSmartPointers created.");
 }
+// 主动调用预导入模块std::mem::drop，就会主动调用，如上例就是先打印c再到d
+// Dropping CustomSmartPointer with data `my stuff`!
+// CustomSmartPointers created.
+// Dropping CustomSmartPointer with data `other stuff`!
+
+
+// 如果不主动调用drop方法，在离开main函数时，会先打印Dropping CustomSmartPointer with data d再到c
+// CustomSmartPointers created.
+// Dropping CustomSmartPointer with data `other stuff`!
+// Dropping CustomSmartPointer with data `my stuff`!
