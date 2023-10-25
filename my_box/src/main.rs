05f6f7ea2908c631be8934b905cce79f69235986
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-25 10:31:43 +0800

    Deref Trait

--- my_box/src/main.rs
+++ my_box/src/main.rs
@@ -1,17 +1,25 @@
-/* fn main() {
-    let b = Box::new(5); // stack存指针，指针指向存在heap的数据
-    println!("b = {}", b);
-} */
-use crate::List::{Cons, Nil};
-// Box<T>只提供了间接存储和heap内存分配的功能
-fn main() {
-    let list = Cons(1,
-        Box::new(Cons(2,
-            Box::new(Cons(3,
-                Box::new(Nil))))));
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
 }
 
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
+impl<T> Deref for MyBox<T> {  // 要使用*y引用，要实现Deref trait（类似于java重写接口的方法）
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0 // 把self元组的第0个元素返回
+    }
+}
+fn main() {
+    let x = 5;
+    // let y = &x;
+    let y = MyBox::new(x); // 用MyBox新建一个y，相当于获取了x的引用
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
