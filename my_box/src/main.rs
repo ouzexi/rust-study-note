04b99a5911129497652c4df0af2e2b5a7a2ef2f8
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-27 09:54:42 +0800

    Rc

--- my_box/src/main.rs
+++ my_box/src/main.rs
@@ -1,26 +1,21 @@
-struct CustomSmartPointer {
-    data: String,
-}
-// 实现Drop Trait，可以自定义当值将要离开作用域时发生的动作，一般用于文件，网络资源释放
-impl Drop for CustomSmartPointer {
-    fn drop(&mut self) {
-        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
-    }
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil
 }
 
 fn main() {
-    let c = CustomSmartPointer{ data: String::from("my stuff") };
-    drop(c); // 主动调用drop后，不会在离开main时被2次调用
-    let d = CustomSmartPointer { data: String::from("other stuff") };
-    println!("CustomSmartPointers created.");
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a)); // 此时a的强引用为1，只有自己引用
+
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a)); // 此时引用值为2，b引用它
+
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a)); // 此时引用值为3，c引用它
+    }
+    println!("count after c goes out of score = {}", Rc::strong_count(&a)); // 此时c离开它的作用域了，释放了，引用剩2个
 }
-// 主动调用预导入模块std::mem::drop，就会主动调用，如上例就是先打印c再到d
-// Dropping CustomSmartPointer with data `my stuff`!
-// CustomSmartPointers created.
-// Dropping CustomSmartPointer with data `other stuff`!
-
-
-// 如果不主动调用drop方法，在离开main函数时，会先打印Dropping CustomSmartPointer with data d再到c
-// CustomSmartPointers created.
-// Dropping CustomSmartPointer with data `other stuff`!
-// Dropping CustomSmartPointer with data `my stuff`!
