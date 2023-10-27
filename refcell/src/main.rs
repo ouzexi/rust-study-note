acf334cf02e21fa9e52b4e7f0be070b562d801f6
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-27 19:05:51 +0800

    面向对象语言特性

--- refcell/src/main.rs
+++ refcell/src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
-    // Rust语言2个并发概念：std::marker::Sync和std::marker::Send这两个trait（标签trait，没有实现方法）
-    // Send允许线程间转移所有权
-    // Sync允许从多线程访问
+    // rust是面向对象的
+    // struct、enum包含数据
+    // impl块为之提供了方法
+    // 但带有方法的struct、enum并没有被称为对象
+    // 封装：pub关键字
+    // 继承：trait方法进行代码共享
+    // 多态：泛型和trait约束（限定参数化多态）
 }
