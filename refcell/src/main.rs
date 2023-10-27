3a86ebf640a4d4cc54a822f81866265d1ce4de64
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-27 18:30:16 +0800

    多线程的发送者发送多个值

--- refcell/src/main.rs
+++ refcell/src/main.rs
@@ -1,15 +1,25 @@
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();  // 发送端的send方法发送数据
-        // println!("{}", val); 这里会报错了，因为val的所有权移交到主线程
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(1));  // 如果是发送多个值，会阻塞线程，看到接收者在等待
+        }
     });
 
-    let received = rx.recv().unwrap(); // 接收端的recv方法接收数据
-    println!("Got: {}", received);
+    for received in rx {  // 这里使用循环解构，就不需要调用recv方法了
+        println!("Got: {}", received);
+    }
 }
