8c9a2835d1ce81e5a128f07e86142e38981c8da9
v-ouzex <v-ouzex.gd@chinatelecom.cn>
2023-10-27 18:56:34 +0800

    使用Sync和Send扩展并发

--- refcell/src/main.rs
+++ refcell/src/main.rs
@@ -1,23 +1,5 @@
-use std::sync::{Mutex, Arc};  // RefCell<T>可以修改Rc<T>里的内容，Mutex<T>可以修改Arc<T>里的内容
-use std::thread;
-// Rc<T>和Arc<T>的api是一样的，但Arc可用户并发情景，Mutex有死锁风险
 fn main() {
-    let counter = Arc::new(Mutex::new(0)); // 共享counter，就可以在循环中的多个线程获取所有权
-    let mut handles = vec![];
-
-    for _ in 0..10 {
-        let counter = Arc::clone(&counter);
-        let handle = thread::spawn(move || {
-            let mut num = counter.lock().unwrap();
-
-            *num += 1;
-        });
-        handles.push(handle);
-    }
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
-
-    println!("Result: {}", *counter.lock().unwrap());
+    // Rust语言2个并发概念：std::marker::Sync和std::marker::Send这两个trait（标签trait，没有实现方法）
+    // Send允许线程间转移所有权
+    // Sync允许从多线程访问
 }
