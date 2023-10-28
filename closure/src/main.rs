8a286ecd1516b70a1058bf8d49fd68d4c3b9997c
区泽熙 <1192102366@qq.com>
2023-10-28 15:18:01 +0800

    可辩驳性-模式(匹配)是否会匹配失败

--- closure/src/main.rs
+++ closure/src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
-    // match Arm
-    // if
-    // while
-    // for
-    // let
-    // 函数参数
+    // 能匹配任何可能传递的值的模式：无可辩驳的 letx = 5;
+    // 对某些可能的值无法进行匹配的模式：可辩驳的 if let Some(x) = a_value;
+
+    // 无可辩驳：函数参数、let、for循环
+    // 可辩驳的：if let、while let
+    let a: Option<i32> = Some(5);
+    // let Some(x) = a; // 此处会报错，因为Some是可辩驳的
 }
