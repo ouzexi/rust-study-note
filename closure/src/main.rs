a46ac208aaeab4c5b0d95c35f3dd44b157db0fab
区泽熙 <1192102366@qq.com>
2023-10-28 17:03:57 +0800

    高级函数和闭包

--- closure/src/main.rs
+++ closure/src/main.rs
@@ -1,18 +1,26 @@
-// 1 类型别名
-type Kilometers = i32;
-
-fn main() {
-    let x: i32 = 5;
-    let y: Kilometers = 5;
-    println!("x + y = {}", x + y);
-
-    // 2 Never类型
-    loop {
-        // 永远不会返回则为Never类型
-    }
+// 1 函数指针
+// 可以将函数传递给其他函数，函数在传递过程中会被强制转换成fn类型
+fn add_one(x: i32) -> i32 {
+    x + 1
 }
 
-// 3 动态大小和Sized Trait
-// fn generic<T>(t: T) {}
-// fn generic<T: Sized>(t: T) {}
-// fn generic<T: ?Sized>(t: &T) {}
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
+}
+
+fn main() {
+    let answer = do_twice(add_one, 5);
+    println!("The answer is: {}", answer); //12
+}
+
+// 2 函数指针与闭包的不同
+// fn是一个类型，不是一个trait
+
+// 3 返回闭包
+/* fn returns_closure() -> Fn(i32) -> i32 {  报错
+    |x| x + 1
+} */
+
+n returns_closure() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
